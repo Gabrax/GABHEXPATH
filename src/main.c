73a8925252cd41655a153648d7cf3201d582651a
Gabrax <gab-oz@wp.pl>
2026-04-04 23:46:54 +0200

    selecting hexes

--- src/main.c
+++ src/main.c
@@ -2,6 +2,59 @@
 #include <math.h>
 #include <stdio.h>
 
+#include "raymath.h"
+
+#define MAX_COLS 10
+#define MAX_ROWS 10
+
+typedef struct {
+    Vector2 center;
+    bool selected;
+} Hex;
+
+Hex grid[MAX_COLS][MAX_ROWS];
+
+void InitHexGrid(int cols, int rows, float radius)
+{
+    float height = sqrtf(3.0f) * radius;
+
+    for (int col = 0; col < cols; col++)
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            float x = col * (radius * 1.5f);
+            float y = row * height + (col % 2) * (height / 2.0f);
+
+            grid[col][row].center = (Vector2){ x + 100, y + 100 };
+            grid[col][row].selected = false;
+        }
+    }
+}
+
+bool IsPointInHex(Vector2 p, Vector2 center, float radius)
+{
+    return Vector2Distance(p, center) < radius;
+}
+
+void HandleInput(int cols, int rows, float radius)
+{
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        Vector2 mouse = GetMousePosition();
+
+        for (int col = 0; col < cols; col++)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                if (IsPointInHex(mouse, grid[col][row].center, radius))
+                {
+                    grid[col][row].selected = !grid[col][row].selected;
+                }
+            }
+        }
+    }
+}
+
 void DrawHexagon(Vector2 center, float radius, Color color)
 {
     Vector2 points[6];
@@ -26,16 +79,13 @@ void DrawHexagon(Vector2 center, float radius, Color color)
 
 void DrawHexGrid(int cols, int rows, float radius)
 {
-    float height = sqrtf(3.0f) * radius;
-
     for (int col = 0; col < cols; col++)
     {
         for (int row = 0; row < rows; row++)
         {
-            float x = (float)col * (radius * 1.5f);
-            float y = (float)row * height + (float)(col % 2) * (height / 2.0f);
+            Color color = grid[col][row].selected ? RED : BLACK;
 
-            DrawHexagon((Vector2){ x + 100, y + 100 }, radius, BLACK);
+            DrawHexagon(grid[col][row].center, radius, color);
         }
     }
 }
@@ -48,12 +98,16 @@ int main()
     InitWindow(screenWidth, screenHeight, "GABHEXPATH");
     SetTargetFPS(60);
 
+    InitHexGrid(MAX_COLS, MAX_ROWS, 15.0f);
+
     while (!WindowShouldClose()) 
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+        HandleInput(MAX_COLS, MAX_ROWS, 15.0f);
 
-        DrawHexGrid(10, 10, 15.0f);
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        DrawHexGrid(MAX_COLS, MAX_ROWS, 15.0f);
 
         EndDrawing();
     }
